08647994bd44f1fc2f9a7fee38a845b213ef52d9
AhmadYusronF <yusronfadilahmad@gmail.com>
2025-11-20 21:23:13 +0700

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
-import Home from './Pages/Home'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+import Home from "./Pages/Home";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
+// asdas
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>      
+    <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home/>} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
