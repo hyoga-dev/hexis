2ab98d699dd4c13f30d8f0a07524e910c0ef0f0e
AhmadYusronF <yusronfadilahmad@gmail.com>
2025-12-06 17:48:11 +0700

    feat: add loading screen component with spinner and styles

--- src/Pages/LoadingScreen.jsx
+++ src/Pages/LoadingScreen.jsx
@@ -1,23 +1,26 @@
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../firebase.js';
 import { useEffect } from 'react';
+import LoadingIcon from "../assets/Icon/LoadingIcon.jsx"
+import Styles from "../assets/Styles/loading.module.css"
 
 const LoadingScreen = () => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (user) => {
-          if (user) {
-              window.location.href = '/habit';
-          } else {
-              window.location.href = '/login';
-          }
+            if (user) {
+                window.location.href = '/habit';
+            } else {
+                window.location.href = '/login';
+            }
         });
-    
+
         return () => unsubscribe();
-      }, []); 
+    }, []);
 
     return (
-        <div className="loading-screen">
-            <div className="spinner"></div>
+        <div className={Styles["loading-screen"]}>
+            <div className={Styles.spinner}></div>
+            <LoadingIcon />
             <p>Loading...</p>
         </div>
     );
