8decc0bf2437c45bd7467836a31922068148ee9b
unknown <hyoga.end>
2025-12-13 11:38:17 +0700

    fix reset password

--- src/data/AuthEmail.jsx
+++ src/data/AuthEmail.jsx
@@ -2,38 +2,40 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } f
 import { useNavigate } from "react-router-dom";
 
 const auth = getAuth();
-const navigate = useNavigate();
+// const navigate = useNavigate();
 
 // const email = "pengguna.baru@contoh.com";
 // const password = "password_kuat_123";
 
 export function signUp(email, password) {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        // console.log("Pendaftaran Berhasil!", user);
-        window.location.href = '/habit';
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.error("Gagal Mendaftar:", errorCode, errorMessage);
-      });
+  const navigate = useNavigate();
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    // console.log("Pendaftaran Berhasil!", user);
+    navigate("/habit")
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.error("Gagal Mendaftar:", errorCode, errorMessage);
+  });
 }
 
 export function signIn(email, password) {
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        navigate("/habit")
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.error("Gagal Login:", errorCode, errorMessage);
+  const navigate = useNavigate();
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      navigate("/habit")
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error("Gagal Login:", errorCode, errorMessage);
 
-        alert("Password atau email salah")
-      });
+      alert("Password atau email salah")
+    });
 }
 
 export function resetPassword(emailPengguna) {
